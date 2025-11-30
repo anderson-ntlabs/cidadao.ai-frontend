dc1b21a6a47954fae1381e8e5e70523a033ae68a
anderson-ufrj <anderson-ufrj@users.noreply.github.com>
2025-11-30 10:45:53 -0300

    feat(survey): add question components barrel export

---
+++ components/survey/questions/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Survey Question Components
+ *
+ * Barrel export for all question type components
+ * @author Anderson Henrique da Silva
+ * @date 2025-11-30
+ */
+
+export { NPSQuestion } from './nps-question'
+export { StarRatingQuestion } from './star-rating-question'
+export { MultipleChoiceQuestion } from './multiple-choice-question'
+export { TextQuestion } from './text-question'
