ccbc12e69c61de3d9e076f64ad1f8b96d15de886
anderson-ufrj <anderson-ufrj@users.noreply.github.com>
2025-11-30 10:53:03 -0300

    feat(badge): add barrel export for badge components
    - Create unified export point for badge components
    - Include indicator and showcase components

---
+++ components/badge/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Badge Components
+ *
+ * Barrel export for all badge-related components
+ * @author Anderson Henrique da Silva
+ * @date 2025-11-30
+ */
+
+export { BadgeIndicator } from './badge-indicator'
+export { BadgeShowcase } from './badge-showcase'
