40c06f6248ea266e3a5cddc41375a391898c0640
anderson-ufrj <anderson-ufrj@users.noreply.github.com>
2025-11-30 10:51:04 -0300

    feat(survey): add barrel export for survey components
    - Create unified export point for all survey components
    - Include modal, progress, success, and FAB components
    - Re-export question components for convenience

---
+++ components/survey/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Survey Components
+ *
+ * Barrel export for all survey-related components
+ * @author Anderson Henrique da Silva
+ * @date 2025-11-30
+ */
+
+export { SurveyModal } from './survey-modal'
+export { SurveyProgress } from './survey-progress'
+export { SurveySuccess } from './survey-success'
+export { SurveyFAB } from './survey-fab'
+
+// Re-export question components
+export { NPSQuestion, StarRatingQuestion, MultipleChoiceQuestion, TextQuestion } from './questions'
